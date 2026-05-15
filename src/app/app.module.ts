b57037c692811d52dc5c65590547178e36e8eeba
Gokul <jgokulkrishnan@outlook.com>
2026-05-15 12:47:53 +0530

    Added signup component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [LoginComponentComponent]
+  bootstrap: [SignupComponent]
 })
 export class AppModule { }
